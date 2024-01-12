4edac1031063652d0ffc4899db07b046c7bc2f80
Soheil Abadifard <abadifardso@gmail.com>
2024-01-12 22:45:54 +0300

    Changes to use DefaultSentimentAnalysisProcessorFactory and SentimentAnalysisProcessorFactory

--- src/main/kotlin/com/github/soheilabadifard/kotlincommentssentimentanalysis/AccessKotlinFilesAction.kt
+++ src/main/kotlin/com/github/soheilabadifard/kotlincommentssentimentanalysis/AccessKotlinFilesAction.kt
@@ -1,16 +1,16 @@
 package com.github.soheilabadifard.kotlincommentssentimentanalysis
 
-import com.github.soheilabadifard.kotlincommentssentimentanalysis.core.SentimentAnalysisProcessor
+import com.github.soheilabadifard.kotlincommentssentimentanalysis.core.SentimentAnalysisProcessorFactory
+import com.github.soheilabadifard.kotlincommentssentimentanalysis.core.DefaultSentimentAnalysisProcessorFactory
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 
-class AccessKotlinFilesAction : AnAction("Analyze KT File's Sentiment") {
+class AccessKotlinFilesAction(private val processorFactory: SentimentAnalysisProcessorFactory = DefaultSentimentAnalysisProcessorFactory()) : AnAction("Analyze KT File's Sentiment") {
 
     override fun actionPerformed(e: AnActionEvent) {
         val project = e.project ?: return
-        val projectPath = project.basePath ?: return
-        val processor = SentimentAnalysisProcessor(project)
+        val processor = processorFactory.create(project)
         processor.performAnalysis()
     }
 }
