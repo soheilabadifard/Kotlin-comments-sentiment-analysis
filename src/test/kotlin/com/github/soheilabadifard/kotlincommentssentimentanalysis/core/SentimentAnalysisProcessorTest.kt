17f7569743b5e1f45f4dd5912719e22b1844c5b0
Soheil Abadifard <abadifardso@gmail.com>
2024-01-12 19:35:07 +0300

    extracted core process test

---
+++ src/test/kotlin/com/github/soheilabadifard/kotlincommentssentimentanalysis/core/SentimentAnalysisProcessorTest.kt
@@ -0,0 +1,90 @@
+package com.github.soheilabadifard.kotlincommentssentimentanalysis.core
+
+import com.intellij.psi.PsiManager
+import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.psi.PsiComment
+import java.nio.file.Files
+import java.nio.file.Path
+import com.intellij.testFramework.fixtures.BasePlatformTestCase
+import kotlinx.coroutines.runBlocking
+
+
+class SentimentAnalysisProcessorTest : BasePlatformTestCase() {
+
+    private lateinit var myAction: SentimentAnalysisProcessor
+
+    override fun setUp() {
+        super.setUp()
+        myAction = SentimentAnalysisProcessor(project)
+
+        // Copy tokenizer resources to the test project
+        copyTokenizerResources()
+        setupVirtualFileSystem()
+    }
+
+    private fun copyTokenizerResources() {
+        // Define the path to your tokenizer resources within your project
+        val tokenizerResourcePath = Path.of("src/test/testdata/")
+
+        // Copy each file from tokenizer Resource Path to the test project's virtual file system
+        Files.walk(tokenizerResourcePath).forEach { filePath ->
+            if (!Files.isDirectory(filePath)) {
+                val relativePath = tokenizerResourcePath.relativize(filePath).toString()
+                val fileContent = Files.readString(filePath)
+                myFixture.addFileToProject(relativePath, fileContent)
+            }
+        }
+    }
+
+    private fun setupVirtualFileSystem() {
+        val kotlinFileContent = """
+                package org.intellij.sdk.kotlin
+
+                import com.intellij.openapi.actionSystem.AnActionEvent
+                import com.intellij.openapi.actionSystem.PlatformDataKeys
+                import com.intellij.openapi.project.DumbAwareAction
+                import com.intellij.openapi.ui.Messages
+                class MyTestFile : DumbAwareAction() {
+                  // positive
+                  override fun actionPerformed(event: AnActionEvent) {
+                    val project = event.getData(PlatformDataKeys.PROJECT)
+                    Messages.showMessageDialog(project, "Hello from Kotlin!", "Greeting", Messages.getInformationIcon())
+                  }
+                
+                }
+            """.trimIndent()
+
+        // Create and write to a Kotlin file in the temporary directory
+        myFixture.addFileToProject("src/main/kotlin/org/intellij/sdk/kotlin/MyTestFile.kt", kotlinFileContent)
+    }
+
+    fun testActionPerformed() {
+        runBlocking {
+            myAction.performAnalysis()
+        }
+
+        // access fileStatisticsMap from myAction
+        val fileStatsMap = myAction.fileStatisticsMap
+
+        val psiFile = myFixture.findFileInTempDir("src/main/kotlin/org/intellij/sdk/kotlin/MyTestFile.kt")
+        assertNotNull("PsiFile should not be null", psiFile)
+
+        val psiManager = PsiManager.getInstance(project)
+        val comments = PsiTreeUtil.findChildrenOfType(psiManager.findFile(psiFile!!), PsiComment::class.java)
+
+        assertFalse("There should be comments in the file", comments.isEmpty())
+
+        // Check if the map is not empty
+        assertFalse("fileStatisticsMap should not be empty", fileStatsMap.isEmpty())
+
+        // For demonstration, let's say you know the expected output for a specific file
+        val expectedFileName = "MyTestFile.kt"
+        val fileStats = fileStatsMap[expectedFileName]
+        assertNotNull("Stats for $expectedFileName should exist", fileStats)
+
+        // Verify the statistics of the file
+        assertTrue("There should be at least one positive comment", fileStats!!.positiveCount > 0)
+        assertTrue("Average probability for positive comments should be within range",
+            fileStats.averagePositiveProbability in 0.0..1.0)
+    }
+}
